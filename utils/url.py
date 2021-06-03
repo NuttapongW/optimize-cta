eea3752e11a79ee699d6e8e4534226891a4d4a72
nuttapongw <nuttapong@netbay.co.th>
2021-06-03 20:58:43 +0700

    add utils

---
+++ utils/url.py
@@ -0,0 +1,13 @@
+
+
+KAFKA_URLS = [
+    "PLAINTEXT://kafka0:9092",
+    "PLAINTEXT://kafka1:9093",
+    "PLAINTEXT://kafka2:9094"
+]
+
+REST_PROXY_URL = "http://rest-proxy:8082/"
+SCHEMA_REGISTRY_URL = "http://schema-registry:8081/"
+KAFKA_CONNECT_URL = "http://kafka-connect:8083"
+KSQL_URL = "http://ksql:8088"
+POSTGRESQL_URL = "jdbc:postgresql://postgres:5432/cta"
